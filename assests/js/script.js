5bb43b763a7eb61a2715997b8c59b52972341cdd
Mie-Nguyn <nguyennhat2k66@gmail.com>
2024-11-20 06:27:37 +0700

    commit login

---
+++ assests/js/script.js
@@ -0,0 +1,21 @@
+const warpper = document.querySelector('.wrapper    ');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const nutlogin= document.querySelector('.btn-log');
+const thoatlogin= document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    warpper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    warpper.classList.remove('active');
+});
+
+nutlogin.addEventListener('click', ()=> {
+    warpper.classList.add('active-popup');
+});
+
+thoatlogin.addEventListener('click', ()=> {
+    warpper.classList.remove('active-popup');
+});
